e634ecad2f5d1a8f447e079348ef4ee3ed07548a
juli0mendes <jmendes@live.com>
2019-10-16 15:28:08 -0300

    first commit

---
+++ src/test/java/br/com/juli0mendes/cars/CarsMsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.juli0mendes.cars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarsMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
